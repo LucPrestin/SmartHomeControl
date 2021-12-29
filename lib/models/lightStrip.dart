ce393f55ae6fde8ba85c3153ba95d575302144ae
LucPrestin <LucPrestin@users.noreply.github.com>
2021-12-29 19:01:09 +0100

    add LightStrip model class

---
+++ lib/models/lightStrip.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LightStrip {
+  static const tableName = "LightStripTable";
+
+  int id;
+  Color color;
+  String name;
+  bool isOn;
+
+  LightStrip(
+      {required this.id,
+      required this.name,
+      this.color = Colors.black,
+      this.isOn = false});
+
+  static fromMap(Map<String, dynamic> map) {
+    return LightStrip(
+        id: map['id'],
+        name: map['name'],
+        color: Color(map['color']),
+        isOn: map['isOn'] == 1);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {"id": id, "color": color.value, "name": name, "isOn": isOn ? 1 : 0};
+  }
+}
