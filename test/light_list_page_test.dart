b0a84e28f8c2c8ef39597f248d059c0d8fbda923
LucPrestin <LucPrestin@users.noreply.github.com>
2022-01-16 21:19:01 +0100

    add tests for the light list page

---
+++ test/light_list_page_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_home_control/main.dart';
+import 'package:smart_home_control/views/light_add_page.dart';
+
+void main() {
+  Future navigateToLightListPage(WidgetTester tester) async {
+    await tester.pumpWidget(const SmartHomeControlApp());
+    await tester.pumpAndSettle();
+
+    await tester.dragFrom(
+        tester.getTopLeft(find.byType(MaterialApp)), const Offset(300, 0));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byIcon(Icons.lightbulb));
+    await tester.pumpAndSettle();
+  }
+
+  group('child components', () {
+    testWidgets('has a FloatingActionButton', (WidgetTester tester) async {
+      await navigateToLightListPage(tester);
+
+      expect(find.byType(FloatingActionButton), findsOneWidget);
+    });
+  });
+  group('routing', () {
+    testWidgets('clicking the fab routes to the LightAddPage',
+        (WidgetTester tester) async {
+      await navigateToLightListPage(tester);
+
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(LightAddPage), findsOneWidget);
+    });
+  });
+}
