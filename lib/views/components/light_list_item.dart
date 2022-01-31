e4358bfca86c599afaf534cd4cade10bf3c04074
LucPrestin <LucPrestin@users.noreply.github.com>
2022-01-31 21:25:55 +0100

    use FutureBuilder to load the light strips and then build the ui

--- lib/views/components/light_list_item.dart
+++ lib/views/components/light_list_item.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:smart_home_control/models/light_strip.dart';
 
 class LightListItem extends StatefulWidget {
+  final LightStrip strip;
+
+  const LightListItem(this.strip, {Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _LightListItemState();
 }
--- lib/views/pages/light_list_page.dart
+++ lib/views/pages/light_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_home_control/models/database.dart';
+import 'package:smart_home_control/models/light_strip.dart';
 
 import 'package:smart_home_control/routes/routes.dart';
 import 'package:smart_home_control/views/components/light_list_item.dart';
@@ -15,21 +17,46 @@ class _LightListPageState extends State<LightListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lights'),
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          shrinkWrap: false,
-          itemCount: 40,
-          itemBuilder: (BuildContext context, int index) {
-            return LightListItem();
-          }),
-      drawer: const NavigationDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, Routes.lightsNew),
-        child: const Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Lights'),
+        ),
+        body: FutureBuilder<List<LightStrip>>(
+          future: DatabaseHelper.instance.getAllLightStrips(),
+          builder: (context, snapshot) {
+            Widget body;
+            if (snapshot.hasData) {
+              var strips = snapshot.data!;
+              body = ListView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  shrinkWrap: false,
+                  itemCount: strips.length,
+                  itemBuilder: (context, index) =>
+                      LightListItem(strips.elementAt(index)));
+            } else if (snapshot.hasError) {
+              body = Column(children: [
+                const Icon(Icons.error_outline),
+                Text(snapshot.error.toString())
+              ]);
+            } else {
+              body = Column(
+                children: const [
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Text('Waiting for database to load the light strips')
+                ],
+              );
+            }
+
+            return body;
+          },
+        ),
+        drawer: const NavigationDrawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, Routes.lightsNew),
+          child: const Icon(Icons.add),
+        ));
   }
 }
