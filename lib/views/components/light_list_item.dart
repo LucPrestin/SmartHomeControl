deb7ad7cd50131675edf2e441da0a32a97f7f138
LucPrestin <LucPrestin@users.noreply.github.com>
2022-01-31 22:22:19 +0100

    add first design of light strip list elements

--- lib/views/components/light_list_item.dart
+++ lib/views/components/light_list_item.dart
@@ -13,6 +13,53 @@ class LightListItem extends StatefulWidget {
 class _LightListItemState extends State<LightListItem> {
   @override
   Widget build(BuildContext context) {
-    return const ListTile(title: Text('list tile'));
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[names(), colorBox(), onOffToggle(), editButton()]);
+  }
+
+  Column names() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            widget.strip.name,
+            style: const TextStyle(color: Color(0xFF000000)),
+          ),
+          Text(
+            widget.strip.mqttId,
+            style: const TextStyle(color: Color(0xFF9d9d9d)),
+          )
+        ]);
+  }
+
+  GestureDetector colorBox() {
+    return GestureDetector(
+        onTap: () {},
+        child: SizedBox(
+            width: 40,
+            height: 40,
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    color: widget.strip.color,
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(4.0)))));
+  }
+
+  Switch onOffToggle() {
+    return Switch(onChanged: (value) {}, value: widget.strip.isOn);
+  }
+
+  IconButton editButton() {
+    return IconButton(
+      icon: const Icon(Icons.edit),
+      onPressed: () {},
+      iconSize: 28.0,
+      color: const Color(0xFF9d9d9d),
+    );
   }
 }
