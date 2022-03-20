77a009844a3c1a851b42b31c646285ec5b9d8329
LucPrestin <l.prestin@gmx.de>
2022-03-20 15:03:40 +0100

    add tests for smart hub list page

---
+++ integration_test/smart_hub_list_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:smart_home_control/main.dart' as app;
+import 'package:smart_home_control/views/components/navigation_drawer.dart';
+
+Future navigateToSmartHubListPage(WidgetTester tester) async {
+  app.main();
+  await tester.pumpAndSettle();
+
+  await tester.tap(find
+      .descendant(
+        of: find.byType(AppBar),
+        matching: find.byType(IconButton),
+      )
+      .first);
+  await tester.pumpAndSettle();
+
+  await tester.tap(find.byKey(const Key('to SmartHubListPage')));
+  await tester.pumpAndSettle();
+}
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('opens navigation drawer on sandwich button tap',
+      (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+
+    Finder drawer = find
+        .descendant(
+          of: find.byType(AppBar),
+          matching: find.byType(IconButton),
+        )
+        .first;
+    await tester.tap(drawer);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(NavigationDrawer), findsOneWidget);
+  });
+}
