20141fbdb20c91cc3ccd85bc6ba843001fb93067
LucPrestin <l.prestin@gmx.de>
2022-03-20 12:44:40 +0100

    Add util method to get device name

---
+++ lib/helpers/utils.dart
@@ -0,0 +1,22 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+
+Future<String?> getDeviceName() async {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+  String? name;
+
+  if (Platform.isAndroid) {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    name = androidInfo.display;
+  } else if (Platform.isIOS) {
+    IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    name = iosInfo.name;
+  } else {
+    // currently not supported
+  }
+
+  return name;
+}
